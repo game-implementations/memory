65f62ec83aab21a103dfa7db8d48f662ab1fd290
Axlfc <axlffcc@gmail.com>
2023-11-10 14:54:35 +0100

    UPDATED: Reverting PlayerController logic to respect MVC separation of concerns

--- src/view/TerminalView.py
+++ src/view/TerminalView.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 from src.controller.GameController import GameController
 from src.controller.PlayerController import PlayerController
+from src.model.Player import Player
+from src.model.Position import Position
 
 
 def display_board_raw(board):
@@ -38,23 +40,68 @@ def display_board(board):
     print(msg)
 
 
+def ask_position(num_columns, num_rows):
+    pos = Position()
+    col_letter = input("Please write column letter:\t")
+    col_letter = col_letter.upper()
+    col_letter = col_letter[0]
+    if ord(col_letter) - ord('A') < 0 or ord(col_letter) - ord('A') >= num_columns:
+        raise ValueError("Out of range letter.")
+    pos.x = ord(col_letter) - ord('A')
+
+    row_number = input("Please write row number:\t")
+    row_number = row_number[0]
+    row_number = int(row_number) - 1
+    if row_number < 0 or row_number >= num_rows:
+        raise ValueError("Out of range number.")
+
+    pos.y = row_number
+
+    return pos
+
+
+def display_player_turn(player):
+    print(f"~ {player.name}'s turn ~")
+
+
+def display_winner(winner):
+    print(f"{winner.name} has won!")
+
+
+def display_draw():
+    print("This game ended in a draw.")
+
+
+def display_score(player):
+    score = int((player.guessedPairs / player.tries) * 100)
+    print(f"~ SCORE of {player.name}: ~\t", score)
+
+
+def initialize_players(num_players):
+    players = []
+    for _ in range(num_players):
+        player = Player()
+        player.name = input(f"Enter the name for Player {_ + 1}: ")
+        players.append(player)
+    return players
+
+
 def main_view_two_players():
     NUM_ROWS = 2
     NUM_COLUMNS = 2
     game_controller = GameController()
-    player_controller = PlayerController()
 
-    players = player_controller.initialize_players(2)
+    players = initialize_players(2)
 
     game = game_controller.initialize_game_two_players(players[0], players[1], NUM_COLUMNS, NUM_ROWS)
 
     display_board(game.board)
     while not game.is_game_finished:
         current_player = game.player_one if game.is_player_one_turn else game.player_two
-        player_controller.display_player_turn(current_player)
+        display_player_turn(current_player)
 
-        pos1 = player_controller.ask_position(NUM_COLUMNS, NUM_ROWS)
-        pos2 = player_controller.ask_position(NUM_COLUMNS, NUM_ROWS)
+        pos1 = ask_position(NUM_COLUMNS, NUM_ROWS)
+        pos2 = ask_position(NUM_COLUMNS, NUM_ROWS)
         game_controller.turn_cards(game.board, pos1.x, pos1.y, pos2.x, pos2.y)
         display_board(game.board)
         game_controller.turn_cards(game.board, pos1.x, pos1.y, pos2.x, pos2.y)
@@ -72,38 +119,37 @@ def main_view_two_players():
             game.is_player_one_turn = not game.is_player_one_turn
 
     if players[0].guessedPairs > players[1].guessedPairs:
-        player_controller.display_winner(players[0])
-        player_controller.display_score(players[0])
+        display_winner(players[0])
+        display_score(players[0])
     elif players[1].guessedPairs > players[0].guessedPairs:
-        player_controller.display_winner(players[1])
-        player_controller.display_score(players[1])
+        display_winner(players[1])
+        display_score(players[1])
     else:
         if players[0].tries > players[1].tries:
-            player_controller.display_winner(players[1])
-            player_controller.display_score(players[1])
+            display_winner(players[1])
+            display_score(players[1])
         elif players[1].tries > players[0].tries:
-            player_controller.display_winner(players[0])
-            player_controller.display_score(players[0])
+            display_winner(players[0])
+            display_score(players[0])
         else:
-            player_controller.display_draw()
-            player_controller.display_score(players[1])
-            player_controller.display_score(players[0])
+            display_draw()
+            display_score(players[1])
+            display_score(players[0])
 
 
 def main_view_one_player():
     NUM_ROWS = 2
     NUM_COLUMNS = 2
     game_controller = GameController()
-    player_controller = PlayerController()
 
-    player = player_controller.initialize_players(1)[0]
+    player = initialize_players(1)[0]
 
     game = game_controller.initialize_game_one_player(player, NUM_COLUMNS, NUM_ROWS)
 
     display_board(game.board)
     while not game.is_game_finished:
-        pos1 = player_controller.ask_position(NUM_COLUMNS, NUM_ROWS)
-        pos2 = player_controller.ask_position(NUM_COLUMNS, NUM_ROWS)
+        pos1 = ask_position(NUM_COLUMNS, NUM_ROWS)
+        pos2 = ask_position(NUM_COLUMNS, NUM_ROWS)
         print("POSITION:\t", pos1)
         print("POSITION2:\t", pos2)
         game_controller.turn_cards(game.board, pos1.x, pos1.y, pos2.x, pos2.y)
@@ -111,6 +157,6 @@ def main_view_one_player():
         game_controller.turn_cards(game.board, pos1.x, pos1.y, pos2.x, pos2.y)
         game_controller.play_turn(game, player, pos1.x, pos1.y, pos2.x, pos2.y)
 
-    player_controller.display_score(player)
+    display_score(player)
 
 
